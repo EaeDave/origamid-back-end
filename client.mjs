061084f38ee5a25806d40df2e57fafc6a49fe5d9
EaeDave <d4vd.sv@gmail.com>
2025-12-24 12:04:15 -0300

    basic node server structure

--- client.mjs
+++ client.mjs
@@ -1,1 +1,9 @@
-console.log('Teste')
+const getResponse = await fetch('http://localhost:3000')
+const bodyGetResponse = await getResponse.text()
+console.log(bodyGetResponse)
+
+const postResponse = await fetch('http://localhost:3000/produto', {
+  method: 'POST',
+})
+const bodyPostResponse = await postResponse.text()
+console.log(bodyPostResponse)
--- server.mjs
+++ server.mjs
@@ -1,3 +1,20 @@
-import { log } from 'node:console'
+import { createServer } from 'node:http'
 
-log('OI')
+const server = createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.statusCode = 200
+    res.end('Home.')
+    console.log(req.method, req.url)
+  } else if (req.method === 'POST' && req.url === '/produto') {
+    res.statusCode = 201
+    res.end('Produto criado.')
+    console.log(req.method, req.url)
+  } else {
+    res.statusCode = 404
+    res.end('Página não encontrada.')
+  }
+})
+
+server.listen(3000, () => {
+  console.log('Server: http://localhost:3000')
+})
