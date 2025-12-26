9e8d485e931a6cc6167444937f17ddb608d7c313
EaeDave <d4vd.sv@gmail.com>
2025-12-26 13:29:16 -0300

    requests 1

--- client.mjs
+++ client.mjs
@@ -1,9 +1,15 @@
 const getResponse = await fetch('http://localhost:3000')
 const bodyGetResponse = await getResponse.text()
-console.log(bodyGetResponse)
+// console.log(bodyGetResponse)
 
-const postResponse = await fetch('http://localhost:3000/produto', {
-  method: 'POST',
-})
+const postResponse = await fetch(
+  'http://localhost:3000/produto?cor=azul&tamanho=g',
+  {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+)
 const bodyPostResponse = await postResponse.text()
 console.log(bodyPostResponse)
--- server.mjs
+++ server.mjs
@@ -1,13 +1,20 @@
 import { createServer } from 'node:http'
 
 const server = createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/') {
+  const url = new URL(req.url, 'http://localhost')
+
+  const cor = url.searchParams.get('cor')
+  const tamanho = url.searchParams.get('tamanho')
+
+  console.log(req.headers['content-type'])
+
+  if (req.method === 'GET' && url.pathname === '/') {
     res.statusCode = 200
     res.end('Home.')
     console.log(req.method, req.url)
-  } else if (req.method === 'POST' && req.url === '/produto') {
+  } else if (req.method === 'POST' && url.pathname === '/produto') {
     res.statusCode = 201
-    res.end('Produto criado.')
+    res.end(`Produtos: ${cor}, ${tamanho}`)
     console.log(req.method, req.url)
   } else {
     res.statusCode = 404
